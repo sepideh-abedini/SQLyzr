b0face7765b1c707b166af4643aa08e717ba02b8
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2024-11-01 22:35:36 -0400

    Modifying multithreading

--- src/evaluation/src/models_runner/din_runner.py
+++ src/evaluation/src/models_runner/din_runner.py
@@ -13,42 +13,8 @@ from src.evaluation.src.models_runner.runner import ModelRunner, execute_command
 DIN_FILE = 'src/models/din/DIN-SQL.py'
 
 
-@dataclass
 class DinRunner(ModelRunner):
-    dataset_dir: str
-    output_dir: str
-    thread_count: int
-    temp: int
 
-    def run(self):
-        threads = []
-        self.split_dataset()
-
-        for k in range(self.thread_count):
-            thread = threading.Thread(target=self.run_model_single_time, args=(k,))
-            thread.start()
-            threads.append(thread)
-
-        for thread in threads:
-            thread.join()
-
-    def split_dataset(self):
-        with (open(os.path.join(self.dataset_dir, "dev.json"), 'r') as file):
-            data = json.load(file)
-            data_len = len(data)
-            chunk_size = math.ceil(data_len / self.thread_count)
-
-            for i in range(self.thread_count):
-                start = i * chunk_size
-                end = start + chunk_size
-                chunked = data[start:end]
-
-                dataset_parent_dir = Path(self.dataset_dir).parent
-                dataset_chunk_dir = f"{dataset_parent_dir}/data_{i}"
-                shutil.copytree(self.dataset_dir, dataset_chunk_dir, dirs_exist_ok=True)
-                dev_json_chunk_path = os.path.join(dataset_chunk_dir, "dev.json")
-                with open(dev_json_chunk_path, 'w') as out_file:
-                    json.dump(chunked, out_file, indent=4)
 
     def run_model_single_time(self, k):
         k_dataset_dir = f"{self.dataset_dir}_{k}/"
@@ -56,24 +22,12 @@ class DinRunner(ModelRunner):
         DIN_command = f"python3 {DIN_FILE} --dataset {k_dataset_dir} --output {k_output_path} --temp {self.temp}"
         execute_command(DIN_command)
 
-    def merge_results(self):
-        all_toks = 0
-        with open (f"{self.output_dir}/results.txt", 'w') as out:
-            for i in range(self.thread_count):
-                with open(f"{self.output_dir}/output_{i}", 'r') as file:
-                    count_lines = file.readlines()
-                    last_line = count_lines[-1]
-                    toks = int(last_line.split(":")[-1])
-                    all_toks += toks
-
-                    out.writelines(count_lines[:-1])
-            out.write(f"tokens:{all_toks}")
 
 
 def main():
     runner = DinRunner(dataset_dir="data/dataset/data", output_dir="data/dataset/output_results/din", thread_count=8,
                        temp=1)
-    # runner.run()
+    runner.run()
     runner.merge_results()
 
 if __name__ == '__main__':
--- src/evaluation/src/models_runner/runner.py
+++ src/evaluation/src/models_runner/runner.py
@@ -1,5 +1,15 @@
+import json
+import math
+import os
+import shutil
 import subprocess
+import threading
+import time
 from abc import ABC, abstractmethod
+from pathlib import Path
+
+import tqdm
+from attr import dataclass
 
 
 def execute_command(command: str):
@@ -13,10 +23,56 @@ def execute_command(command: str):
         raise subprocess.CalledProcessError(p.returncode, p.args)
 
 
-
+@dataclass
 class ModelRunner(ABC):
+    dataset_dir: str
+    output_dir: str
+    thread_count: int
+    temp: int
+
+    def run(self):
+        threads = []
+        self.split_dataset()
+
+        for k in range(self.thread_count):
+            thread = threading.Thread(target=self.run_model_single_time, args=(k,))
+            thread.start()
+            threads.append(thread)
+
+        for thread in tqdm.tqdm(threads):
+            thread.join()
+
+    def split_dataset(self):
+        with (open(os.path.join(self.dataset_dir, "dev.json"), 'r') as file):
+            data = json.load(file)
+            data_len = len(data)
+            chunk_size = math.ceil(data_len / self.thread_count)
+
+            for i in range(self.thread_count):
+                start = i * chunk_size
+                end = start + chunk_size
+                chunked = data[start:end]
+
+                dataset_parent_dir = Path(self.dataset_dir).parent
+                dataset_chunk_dir = f"{dataset_parent_dir}/data_{i}"
+                shutil.copytree(self.dataset_dir, dataset_chunk_dir, dirs_exist_ok=True)
+                dev_json_chunk_path = os.path.join(dataset_chunk_dir, "dev.json")
+                with open(dev_json_chunk_path, 'w') as out_file:
+                    json.dump(chunked, out_file, indent=4)
 
     @abstractmethod
-    def run(self, temp):
+    def run_model_single_time(self, k):
         pass
 
+    def merge_results(self):
+        all_toks = 0
+        with open(f"{self.output_dir}/results.txt", 'w') as out:
+            for i in range(self.thread_count):
+                with open(f"{self.output_dir}/output_{i}", 'r') as file:
+                    count_lines = file.readlines()
+                    last_line = count_lines[-1]
+                    toks = int(last_line.split(":")[-1])
+                    all_toks += toks
+
+                    out.writelines(count_lines[:-1])
+            out.write(f"tokens:{all_toks}")
