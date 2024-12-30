8fac335d87b5233ab27105c5c97ccaf7c93bd5a3
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2024-12-29 21:57:17 -0500

    before BIG changes

--- bar.sql
+++
@@ -1,6 +0,0 @@
-SELECT e.name
-FROM (SELECT e.name, o.city
-      FROM employees e
-               JOIN offices o
-                    ON e.office_id = o.id) subquery
-WHERE subquery.city = 'London';
