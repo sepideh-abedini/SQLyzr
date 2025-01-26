8cbc25f7f765f39ec14107264692263565abeaa2
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2025-01-25 21:24:24 -0500

    add resource usage

--- src/sqlyzr/file_sender_usage.py
+++ src/sqlyzr/file_sender_usage.py
@@ -1,9 +1,26 @@
-from pydantic import BaseModel
+import os
+
+import psutil
+from pydantic import BaseModel, computed_field
 
 
 class FileGeneratorUsage(BaseModel):
     total_time: float = 0
     total_tokens: int = 0
+
+    @computed_field
+    @property
+    def mem_mb(self) -> float:
+        return self.__process.memory_info()[0] / float(2 ** 20)
+
+    @computed_field
+    @property
+    def cpu_time(self) -> float:
+        return sum(self.__process.cpu_times())
+
+    @property
+    def __process(self):
+        return psutil.Process(os.getpid())
 
     def __add__(self, other):
         if not isinstance(other, FileGeneratorUsage):
