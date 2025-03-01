74556b5631c85e09029c31cd079c835592e1c48c
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2025-03-01 15:21:22 -0500

    Add more log files

--- src/util/log_util.py
+++ src/util/log_util.py
@@ -5,15 +5,20 @@ from typing import Callable
 from loguru import logger
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
-print("############### LOG_LEVEL: ", LOG_LEVEL, " ###################")
 
 
 def configure_logging():
     logger.remove(0)
     # logger.add(sys.stderr, level=os.environ.get("LOG_LEVEL", "INFO").upper(), colorize=True,
     #            format="<green>{time:HH:mm:ss} | </green><level> {level}: {message}</level>")
-    logger.add(sys.stderr, level=LOG_LEVEL, colorize=True, enqueue=True,
-               format="<green>{time:HH:mm:ss} | </green><cyan>[{module}:{function}:{line}]</cyan><level> {level}: {message}</level>")
+    logger.add("info.log", level="INFO")
+    logger.add("trace.log", level="DEBUG")
+    logger.add("debug.log", level="DEBUG")
+    logger.add("db.trace.log", level="TRACE",filter="src.rel.db_facade")
+    logger.add("db.debug.log", level="DEBUG",filter="src.rel.db_facade")
+    logger.add("post_process.trace.log", level="TRACE",filter="src.third_party.dail.utils.post_process")
+    # logger.add(sys.stderr, level=LOG_LEVEL, colorize=True, enqueue=True, format="<green>{time:HH:mm:ss} | </green><cyan>[{module}:{function}:{line}]</cyan><level> {level}: {message}</level>")
+    logger.add(sys.stderr, level=LOG_LEVEL, colorize=True, enqueue=True, format="<green>{time:HH:mm:ss} | </green><level> {level}: {message}</level>")
 
 
 def trace(func: Callable):
