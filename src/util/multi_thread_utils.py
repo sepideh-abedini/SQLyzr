88385b15bdb2d43ae4272e617b9211ac979dcffd
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2025-03-01 15:16:13 -0500

    Delete multi thread functions

--- src/util/multi_thread_utils.py
+++ src/util/multi_thread_utils.py
@@ -2,29 +2,16 @@ import os
 import shutil
 import threading
 from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures.process import ProcessPoolExecutor
-from typing import Callable, TypeVar, List
 from multiprocessing import Pool
+from typing import Callable, TypeVar, List
 
 from loguru import logger
-from torchgen.gen_functionalization_type import return_str
 from tqdm import tqdm
 
 from src.util.log_util import configure_logging
 
-NUM_THREADS = int(os.environ.get("NUM_THREADS", 1))
-NUM_PROC_CHUNKS = int(os.environ.get("NUM_PROC_CHUNKS"), 1)
-
-logger.info(f"NUM_THREADS={NUM_THREADS}")
-
-TMP_DIR = "./tmp"
-
-try:
-    shutil.rmtree(TMP_DIR, ignore_errors=True)
-    if not os.path.exists(TMP_DIR):
-        os.mkdir(TMP_DIR)
-except Exception:
-    pass
+NUM_PROCS = int(os.environ.get("NUM_PROCS", 1))
+NUM_PROC_CHUNKS = int(os.environ.get("NUM_PROC_CHUNKS", 1))
 
 
 def flatten(lst):
@@ -37,19 +24,17 @@ def chunk_list(lst, k):
     return [lst[i:i + size] for i in range(0, len(lst), size)]
 
 
-def get_thread_pool(num_threads: int = NUM_THREADS):
-    return ThreadPoolExecutor(max_workers=num_threads)
-
-
 T = TypeVar('T')
 U = TypeVar('U')
 
 
 def process_initializer():
     configure_logging()
+    logger.info(f"Started new process: {os.getpid()}")
 
 
-def exec_multi_process(fun: Callable[[List[T]], List[U]], vals: List[T], num_procs: int = NUM_PROC_CHUNKS):
+def exec_multi_process(fun: Callable[[List[T]], List[U]], vals: List[T], num_procs: int = NUM_PROC_CHUNKS,
+                       desc: str = ""):
     chunks = chunk_list(vals, num_procs)
     with Pool(num_procs, initializer=process_initializer) as pool:
         result_chunks = list(pool.map(fun, chunks))
@@ -57,27 +42,13 @@ def exec_multi_process(fun: Callable[[List[T]], List[U]], vals: List[T], num_pro
     return results
 
 
-def exec_multi_process_flat(fun: Callable[[List[T]], List[U]], vals: List[T], num_procs: int):
+def exec_multi_process_flat(fun: Callable[[T], List[U]], vals: List[T], num_procs: int = NUM_PROCS, desc: str = ""):
     results = []
-    with Pool(num_procs, initializer=process_initializer) as p, tqdm(total=len(vals)) as pbar:
+    with Pool(num_procs, initializer=process_initializer) as p, tqdm(total=len(vals), desc=desc) as pbar:
         for result in p.imap(fun, vals):
             pbar.update()
             pbar.refresh()
             results.append(result)
-    return results
-
-
-def exec_multi_thread(fun: Callable[[List[T]], List[U]], vals: List[T], num_threads: int = NUM_THREADS):
-    chunks = chunk_list(vals, num_threads)
-    with get_thread_pool(num_threads) as executor:
-        result_chunks = list(executor.map(fun, chunks))
-    results = flatten(result_chunks)
-    return results
-
-
-def exec_multi_thread_flat(fun: Callable[[T], U], vals: List[T], num_threads: int = NUM_THREADS) -> List[U]:
-    with get_thread_pool(num_threads) as executor:
-        results = list(executor.map(fun, vals))
     return results
 
 
