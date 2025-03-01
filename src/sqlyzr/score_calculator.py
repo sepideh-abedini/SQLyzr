a6be52528d5ad210df5f64aec04f1e042f74a244
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2025-03-01 15:15:08 -0500

    Use multiprocessing for score calculation

--- src/sqlyzr/score_calculator.py
+++ src/sqlyzr/score_calculator.py
@@ -9,29 +9,35 @@ from src.configs.sqlyzr_config import SQLyzrConfig
 from src.eval.metrics import *
 from src.eval.model_eval_config import ModelEvalConfig
 from src.sqlyzr.pred_gold_reader import PredGoldReader
-from src.util.multi_thread_utils import exec_multi_process
-
-EVAL_PROCS = int(os.getenv("EVAL_PROCS", 1))
+from src.util.multi_thread_utils import exec_multi_process, exec_multi_process_flat
 
 
-def calc_for_data(eval_conf, run_conf, data):
-    catter = Catter()
-    scores = []
+def calc_for_entry(eval_conf, run_conf, entry):
+    pred, gold, db_id = entry
     metrics = []
     for metric_name, metric_class in eval_conf.metrics.items():
         metrics.append(metric_class(metric_name, eval_conf.dataset_config))
+    catter = Catter()
+    cat, sub_cat = catter.categorize(gold)
+    example_scores = {"tmp": run_conf.temp, "itr": run_conf.itr, "cat": str(cat), "sub_cat": sub_cat}
+    for metric in metrics:
+        try:
+            score = metric.calc(gold, pred, db_id)
+        except Exception as e:
+            logger.debug(e)
+            score = 0
+        example_scores[metric.name] = score
+    return example_scores
+
+
+def calc_for_data(eval_conf, run_conf, data):
+    scores = []
     for i, (pred, gold, db_id) in tqdm(enumerate(data), total=len(data),
-                                       desc=f"Calculating scores for {run_conf}-{threading.get_ident()} -{os.getpid()}"):
-        cat, sub_cat = catter.categorize(gold)
-        example_scores = {"tmp": run_conf.temp, "itr": run_conf.itr, "cat": str(cat), "sub_cat": sub_cat}
-        for metric in metrics:
-            try:
-                score = metric.calc(gold, pred, db_id)
-            except Exception as e:
-                logger.debug(e)
-                score = 0
-            example_scores[metric.name] = score
+                                       desc=f"Calculating scores for {run_conf}-{os.getpid()}"):
+        example_scores = calc_for_entry(eval_conf, run_conf, (pred, gold, db_id))
         scores.append(example_scores)
+
+    logger.info(f"Calc done: {os.getpid()}")
     return scores
 
 
@@ -43,8 +49,12 @@ def calc_for_conf(config: ModelEvalConfig, conf: SingleRunConfig):
 
     reader = PredGoldReader(conf)
     all_data = reader.get_pred_gold_db_id()
+    # all_data = all_data[4500:4800]
     logger.info(f"Calculating scores for {conf}")
-    all_scores = exec_multi_process(partial(calc_for_data, config, conf), all_data, EVAL_PROCS)
+    all_scores = exec_multi_process_flat(partial(calc_for_entry, config, conf), all_data)
+    # all_scores = exec_multi_process(partial(calc_for_data, config, conf), all_data)
+    # all_scores = list(tqdm(map(partial(calc_for_entry, config, conf), all_data), total=len(all_data)))
+    logger.info(f"Score calculation done {conf}")
     ti_df = pd.DataFrame(all_scores)
     ti_df.to_csv(scores_path)
 
