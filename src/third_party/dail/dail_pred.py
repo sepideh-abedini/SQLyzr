1f925d2891714202e9d9a5239341ac57df73b12c
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2025-03-01 15:07:43 -0500

    - Use multiprocessing for sql post-processing

--- src/third_party/dail/dail_pred.py
+++ src/third_party/dail/dail_pred.py
@@ -1,7 +1,6 @@
 import json
 import os
-import re
-from typing import List, Tuple
+from typing import List
 
 import tqdm
 from loguru import logger
@@ -14,12 +13,9 @@ from src.pred.predictor import Predictor
 from src.third_party.dail.dail_conf import DailConfig
 from src.third_party.dail.data_preprocess import DailSchemaLinksGenerator
 from src.third_party.dail.generate_question import DailQuestionGenerator
-from src.third_party.dail.generate_second_question import DailSecondQuestionGenerator
-from src.third_party.dail.utils.post_process import process_duplication, get_sqls
+from src.third_party.dail.sql_post_processor import DailSqlPostProcessorWorker
 from src.util.model_utils import read_jsonl
-from src.util.multi_thread_utils import chunk_list, flatten, get_thread_pool
-
-DAIL_THREADS = int(os.environ.get("DAIL_THREADS", 1))
+from src.util.multi_thread_utils import exec_multi_process_flat, exec_multi_process
 
 
 class DailPredictor(Predictor):
@@ -44,10 +40,10 @@ class DailPredictor(Predictor):
         self.__load_questions(self.__conf.questions_path())
         self._gen_batch_file(self.__conf.get_path("in"), self.__gen_sql_req)
         await self._ask_file(self.__conf.get_path("in"), self.__conf.get_path("out"))
-        logger.info("SQL generation done!")
+        logger.info("SQL prediction done!")
 
         if os.path.exists(self.__conf.pre_test_result_path()):
-            logger.debug(f"Pre test result exists: {self.__conf.pre_test_result_path()}")
+            logger.info(f"Pre test result exists: {self.__conf.pre_test_result_path()}")
             with open(self.__conf.pre_test_result_path()) as file:
                 sqls = file.readlines()
         else:
@@ -59,9 +55,10 @@ class DailPredictor(Predictor):
             lap = timer.lap()
             with open(f"{self.__conf.pre_test_result_path()}.usage", "w") as usage_file:
                 usage_file.write(f"{lap}")
+        logger.info("SQL post porcessing done!")
 
-        second_question_gen = DailSecondQuestionGenerator(self.__conf, self._run_conf,
-                                                          self.__conf.second_questions_path())
+        second_question_gen = DailQuestionGenerator(self.__conf, self._run_conf,
+                                                    self.__conf.second_questions_path(), second_stage=True)
         usage = second_question_gen.run()
         self._tracker.add_usage(usage)
         logger.info("Second Question generation done!")
@@ -84,67 +81,14 @@ class DailPredictor(Predictor):
         questions_json = json.load(open(path, "r"))
         self.__questions = [_["prompt"] for _ in questions_json["questions"]]
 
-    @staticmethod
-    def __process_gpt_4o_mini_sql(content: str) -> str:
-        content = content.strip()
-        content = content.replace("\n", " ")
-        if "SQL:" in content:
-            pattern = r'.*SQL:[\s`]*(SELECT.*)[\s`]*'
-            content = re.sub(pattern, r'\1', content, flags=re.DOTALL)
-        if "```sql" in content:
-            pattern = r'.*```sql(.*)```.*'
-            content = re.sub(pattern, r'\1', content, flags=re.DOTALL)
-        content = content.strip()
-        return content
-
-    def post_process_response(self, pairs: List[Tuple[str, ChatCompletion]]):
-        results = []
-        for i, pair in tqdm.tqdm(enumerate(pairs), total=len(pairs),
-                                 desc=f"Processing SQLs {self._run_conf}"):
-            db_id = pair[0]
-            response = pair[1]
-            contents = [choice.message.content for choice in response.choices]
-            if self.__conf.gpt_params['n'] == 1:
-                for sql in contents:
-                    sql = " ".join(sql.replace("\n", " ").split())
-                    sql = process_duplication(sql)
-                    if sql.startswith("SELECT"):
-                        results.append(sql + "\n")
-                    elif sql.startswith(" "):
-                        results.append("SELECT" + sql + "\n")
-                    else:
-                        results.append("SELECT " + sql + "\n")
-            else:
-                sqls = contents
-                processed_sqls = []
-                for sql in sqls:
-                    sql = " ".join(sql.replace("\n", " ").split())
-                    sql = process_duplication(sql)
-                    sql = self.__process_gpt_4o_mini_sql(sql)
-                    if sql.startswith("SELECT"):
-                        pass
-                    elif sql.startswith(" "):
-                        sql = "SELECT" + sql
-                    else:
-                        sql = "SELECT " + sql
-                    processed_sqls.append(sql)
-                result = {
-                    'db_id': db_id,
-                    'p_sqls': processed_sqls
-                }
-                final_sqls = get_sqls([result], self.__conf.gpt_params['n'],
-                                      self._run_conf.dataset_config)
-                results.extend(final_sqls)
-        return results
-
     def __process_responses(self, file_path) -> List[str]:
         with open(self._run_conf.dataset_config.get_test_path()) as data_file:
             data = json.load(data_file)
             db_ids = [example['db_id'] for example in data]
         responses = read_jsonl(file_path, ChatCompletion)
         pairs = list(zip(db_ids, responses))
-        chunks = chunk_list(pairs, DAIL_THREADS)
-        with get_thread_pool(DAIL_THREADS) as executor:
-            result_chunks = list(executor.map(self.post_process_response, chunks))
-        results = flatten(result_chunks)
+        post_process_worker = DailSqlPostProcessorWorker(self.__conf, self._run_conf)
+        results = exec_multi_process_flat(post_process_worker.post_proc_single, pairs, desc="Post processing SQLs")
+        # results = exec_multi_process(post_process_worker.post_process_response, pairs)
+        # results = list(tqdm.tqdm(map(post_process_worker.post_proc_single, pairs),total=len(pairs)))
         return results
