cd5642f2360469f61e6fdfb6a31744a490687492
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2025-03-02 11:37:53 -0500

    Fix log util

--- src/util/log_util.py
+++ src/util/log_util.py
@@ -8,17 +8,21 @@ LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
 
 
 def configure_logging():
-    logger.remove(0)
+    try:
+        logger.remove(0)
+    except Exception:
+        pass
     # logger.add(sys.stderr, level=os.environ.get("LOG_LEVEL", "INFO").upper(), colorize=True,
     #            format="<green>{time:HH:mm:ss} | </green><level> {level}: {message}</level>")
     logger.add("info.log", level="INFO")
     logger.add("trace.log", level="DEBUG")
     logger.add("debug.log", level="DEBUG")
-    logger.add("db.trace.log", level="TRACE",filter="src.rel.db_facade")
-    logger.add("db.debug.log", level="DEBUG",filter="src.rel.db_facade")
-    logger.add("post_process.trace.log", level="TRACE",filter="src.third_party.dail.utils.post_process")
+    logger.add("db.trace.log", level="TRACE", filter="src.rel.db_facade")
+    logger.add("db.debug.log", level="DEBUG", filter="src.rel.db_facade")
+    logger.add("post_process.trace.log", level="TRACE", filter="src.third_party.dail.utils.post_process")
     # logger.add(sys.stderr, level=LOG_LEVEL, colorize=True, enqueue=True, format="<green>{time:HH:mm:ss} | </green><cyan>[{module}:{function}:{line}]</cyan><level> {level}: {message}</level>")
-    logger.add(sys.stderr, level=LOG_LEVEL, colorize=True, enqueue=True, format="<green>{time:HH:mm:ss} | </green><level> {level}: {message}</level>")
+    logger.add(sys.stderr, level=LOG_LEVEL, colorize=True, enqueue=True,
+               format="<green>{time:HH:mm:ss} | </green><level> {level}: {message}</level>")
 
 
 def trace(func: Callable):
