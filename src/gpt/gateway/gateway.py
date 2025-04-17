264f260b7345b2327941a6d12d2943404dd20d55
Sepideh Abedini <sepideh.abedini.20@gmail.com>
2025-04-16 21:38:43 -0400

    Add cache for GPT

--- src/gpt/gateway/gateway.py
+++ src/gpt/gateway/gateway.py
@@ -1,6 +1,7 @@
 import os
 
 import backoff
+from diskcache import Cache
 from openai import AsyncClient, RateLimitError
 from openai.types.chat import ChatCompletion
 
@@ -8,6 +9,10 @@ from src.gpt.gateway.gateway_exceptions import GptRateLimitException
 from src.gpt.gateway.single.token_tracker import GptTokenTracker
 from src.gpt.models import BatchInputRequest
 from loguru import logger
+
+cache = Cache("/tmp/gpt_cache")
+
+GPT_CACHE = bool(int(os.environ.get("GPT_CACHE", 0)))
 
 
 class GptGateway:
@@ -24,6 +29,10 @@ class GptGateway:
 
     @backoff.on_exception(backoff.constant, interval=10, max_tries=100, exception=GptRateLimitException)
     async def track_and_send(self, request: BatchInputRequest) -> ChatCompletion:
+        s = str(request)
+        if GPT_CACHE:
+            if s in cache:
+                return cache[s]
         logger.debug(f"Sending [{request.custom_id}]")
         tokens = request.get_token_usage()
         can_send = await self.__tracker.check_limit(tokens)
@@ -32,6 +41,8 @@ class GptGateway:
             result = await self.send_without_tracking(request)
             logger.debug(f"TOKENS DIFF: {tokens}@{result.usage.total_tokens}")
             usage.expire()
+            if GPT_CACHE:
+                cache[s] = result
             return result
         else:
             raise GptRateLimitException()
